c5b57c6e7dc4c0e54f69ee371302e8145ce07683
Tari Puspita <241230062@unmuhpnk.ac.id>
2026-07-16 18:49:23 +0700

    ig

--- includes/footer.php
+++ includes/footer.php
@@ -29,7 +29,12 @@
                     <i class="fas fa-map-marker-alt me-2"></i> Jl.Tanray 2 Gg.Nusa Abadi No. 22b
                 </p>
                 <p class="small text-secondary mb-0">
-                    <i class="fas fa-phone me-2"></i> +62 812 3456 7890
+                    <i class="fas fa-phone me-2"></i> +62 815 2296 9194
+                </p>
+                <p class="small text-secondary mb-0 mt-2">
+                    <a href="https://instagram.com/merayauadventure" target="_blank" class="text-decoration-none text-secondary">
+                        <i class="fab fa-instagram fa-fw me-2"></i> @merayauadventure
+                    </a>
                 </p>
             </div>
         </div>
